ba87010a242220111c75b3cd98ee2a33e2306b86
ohmyoungjin <sisaudwls@naver.com>
2022-11-03 13:21:35 +0900

    20221103 :: MVC patten V3 (Model add) 2

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -35,6 +35,9 @@ public class MyView {
         //model에 담겨진 회원 목록 저장 및 회원 목록을 임의로 생성한 modelMap 에서 request Model 으로 옮김
         //***말 그대로 model 자체를 옮긴다 안에 있는 값을 일일이 옮기는 것이 아닌 model 자체 주소 값을 옮김
         //이렇게 생성된 request model 은 view 에 그려지게 된다.
+        //JSP는 request.getAttribute() 로 데이터를 조회하기 때문에, 모델의 데이터를 꺼내서
+        //request.setAttribute() 로 담아둔다.
+        //JSP로 포워드 해서 JSP를 렌더링 한다.
         System.out.println("modelToRequestAttribute start !!");
         //model이 {} 값으로 없으면 로직자체를 실행시키지 않는다 nullPoint Exception이 안나는게 신기함
         model.forEach((key, value)-> System.out.println("model key : " + key + " model value : " + value));
